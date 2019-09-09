f4943663c5b8b98fbaf4643374040044a5155fe9
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-09-10 01:39:30 +0300

    29 урок. Настроили перемещение треков на экране Library

--- IMusic/Library.swift
+++ IMusic/Library.swift
@@ -57,6 +57,21 @@ struct Library: View {
                                 }
                                 .simultaneously(with: TapGesture()
                                     .onEnded { _ in
+//                                        let keyWindow = UIApplication.shared.connectedScenes.filter({
+//                                            $0.activationState == .foregroundActive
+//                                        }).map({$0 as? UIWindowScene}).compactMap({
+//                                            $0
+//                                            }).first?.windows.filter({$0.isKeyWindow}).first
+                                        let keyWindow = UIApplication.shared.connectedScenes
+                                        .filter({$0.activationState == .foregroundActive})
+                                        .map({$0 as? UIWindowScene})
+                                        .compactMap({$0})
+                                        .first?.windows
+                                        .filter({$0.isKeyWindow}).first
+                                        let tabBarVC = keyWindow?.rootViewController as? MainTabBarController
+                                        tabBarVC?.trackDetailView.delegate = self
+                                        
+                                        
                                         self.track = track
                                         self.tabBarDelegate?.maximizeTrackDetailController(viewModel: self.track)
                                     }))
@@ -114,5 +129,33 @@ struct LibraryCell: View {
 struct Library_Previews: PreviewProvider {
     static var previews: some View {
         Library()
+    }
+}
+
+extension Library: TrackMovingDelegate {
+    func moveBackForPreviousTrack() -> SearchViewModel.Cell? {
+        let index = tracks.firstIndex(of: track)
+        guard let myIndex = index else { return nil }
+        var nextTrack: SearchViewModel.Cell
+        if myIndex - 1 == -1 {
+            nextTrack = tracks[tracks.count - 1]
+        } else {
+            nextTrack = tracks[myIndex - 1]
+        }
+        self.track = nextTrack
+        return nextTrack
+    }
+    
+    func moveForwardForPreviousTrack() -> SearchViewModel.Cell? {
+        let index = tracks.firstIndex(of: track)
+        guard let myIndex = index else { return nil }
+        var nextTrack: SearchViewModel.Cell
+        if myIndex + 1 == tracks.count {
+            nextTrack = tracks[0]
+        } else {
+            nextTrack = tracks[myIndex + 1]
+        }
+        self.track = nextTrack
+        return nextTrack
     }
 }

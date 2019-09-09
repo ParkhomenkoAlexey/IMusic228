6f813b1c773504221c6130c4dceee96b3a47d62a
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-09-09 21:37:14 +0300

    27 урок. Передали треки в Library View

--- IMusic/Library.swift
+++ IMusic/Library.swift
@@ -7,8 +7,12 @@
 //
 
 import SwiftUI
+import URLImage
 
 struct Library: View {
+    
+    var tracks = UserDefaults.standard.savedTracks()
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -36,10 +40,8 @@ struct Library: View {
                 }.padding().frame(height: 50)
                 Divider().padding(.leading).padding(.trailing)
                 
-                List {
-                    LibraryCell()
-                    Text("Second")
-                    Text("Third")
+                List(tracks) { track in
+                    LibraryCell(cell: track)
                 }
             }
                 .navigationBarTitle("Library")
@@ -49,12 +51,15 @@ struct Library: View {
 }
 
 struct LibraryCell: View {
+    
+    var cell: SearchViewModel.Cell
+    
     var body: some View {
         HStack {
-            Image("Image").resizable().frame(width: 60, height: 60).cornerRadius(2)
-            VStack {
-                Text("Track Name")
-                Text("Artist Name")
+            URLImage(URL(string: cell.iconUrlString ?? "")!).resizable().frame(width: 60, height: 60).cornerRadius(2)
+            VStack(alignment: .leading) {
+                Text("\(cell.trackName)")
+                Text("\(cell.artistName)")
             }
         }
         
--- IMusic/Search/Cell/TrackCell.swift
+++ IMusic/Search/Cell/TrackCell.swift
@@ -30,6 +30,8 @@ class TrackCell: UITableViewCell {
         super.awakeFromNib()
     }
     
+    @IBOutlet weak var addTrackOutlet: UIButton!
+    
     override func prepareForReuse() {
         super.prepareForReuse()
         
@@ -39,8 +41,20 @@ class TrackCell: UITableViewCell {
     var cell: SearchViewModel.Cell?
     
     func set(viewModel: SearchViewModel.Cell) {
-
+        
         self.cell = viewModel
+        
+        let savedTracks = UserDefaults.standard.savedTracks()
+        let hasFavourite = savedTracks.firstIndex(where: {
+            $0.trackName == self.cell?.trackName && $0.artistName == self.cell?.artistName
+        }) != nil
+        if hasFavourite {
+            addTrackOutlet.isHidden = true
+        } else {
+            addTrackOutlet.isHidden = false
+        }
+
+        
         trackNameLabel.text = viewModel.trackName
         artistNameLabel.text = viewModel.artistName
         collectionNameLabel.text = viewModel.collectionName
@@ -51,24 +65,20 @@ class TrackCell: UITableViewCell {
     
     
     @IBAction func addTrackAction(_ sender: Any) {
-//        print("444")
+
         let defaults = UserDefaults.standard
-//        defaults.set(25, forKey: "Age")
-//        defaults.set("hello", forKey: "String")
+        guard let cell = cell else { return }
+        addTrackOutlet.isHidden = true
         
-        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: cell, requiringSecureCoding: false) {
+        var listOfTracks = defaults.savedTracks()
+        
+        listOfTracks.append(cell)
+        
+        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: listOfTracks, requiringSecureCoding: false) {
             print("Успешно!")
-            defaults.set(savedData, forKey: "tracks")
+            defaults.set(savedData, forKey: UserDefaults.favouriteTrackKey)
         }
     }
     
-    @IBAction func showInfoAction(_ sender: Any) {
-        let defaults = UserDefaults.standard
-        if let savedTrack = defaults.object(forKey: "tracks") as? Data {
-            if let decodedTracks = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(savedTrack) as? SearchViewModel.Cell {
-                print("decodedTracks.trackName: \(decodedTracks.trackName)")
-            }
-        }
-        
-    }
+
 }
